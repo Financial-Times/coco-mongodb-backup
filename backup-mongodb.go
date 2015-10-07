a0d742ca305cc4d22f114ef4939022a0b88117a1
Tamas Molnar <tamas.molnar@ft.com>
2015-10-07 15:52:22 +0300

    Extracted MongoService in a separate file

--- backup-mongodb.go
+++ backup-mongodb.go
@@ -8,61 +8,10 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/rlmcpherson/s3gof3r"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
-
-type mongoService struct {
-	connectionString, database string
-	session                    *mgo.Session
-}
-
-func buildMongoConnectionString(host string, port int, connectionOptions []string) string {
-	mongoConnectionString := host + ":" + strconv.Itoa(port)
-	if len(connectionOptions) == 0 {
-		return mongoConnectionString
-	}
-
-	mongoConnectionString += "/?"
-	for _, param := range connectionOptions {
-		mongoConnectionString += param + connectionOptionSeparator
-	}
-	mongoConnectionString = strings.TrimSuffix(mongoConnectionString, connectionOptionSeparator)
-
-	return mongoConnectionString
-}
-
-func newMongoService(host string, port int, connectionOptions []string, database string) *mongoService {
-	return &mongoService{buildMongoConnectionString(host, port, connectionOptions), database, nil}
-}
-
-func (service *mongoService) openSession() {
-	session, err := mgo.Dial(service.connectionString)
-	if err != nil {
-		log.Panic("Can't connect to mongo on "+service.connectionString, err.Error(), err)
-	}
-	session.SetMode(mgo.Monotonic, true)
-	service.session = session
-}
-
-func (service *mongoService) closeSession() {
-	service.session.Close()
-}
-
-func (service *mongoService) isCurrentNodeMaster() bool {
-	db := service.session.DB(defaultDb)
-	result := make(map[string]interface{})
-	err := db.Run(bson.M{"isMaster": 1}, result)
-	if err != nil {
-		log.Panic("Can't check if node is master, db.isMaster() fails", err.Error(), err)
-	}
-	return result["ismaster"].(bool)
-}
 
 func readArgs() (string, int, string, string, string, string, string) {
 	mongoDbHost := flag.String("mongoDbHost", "", "Mongo DB Host")
@@ -90,7 +39,7 @@ func abortOnInvalidParams(paramNames []string) {
 	for _, paramName := range paramNames {
 		warn.Println(paramName + " is missing or invalid!")
 	}
-	log.Panic("Aborting backulp operation!")
+	log.Panic("Aborting backup operation!")
 }
 
 func checkIfArgsAreEmpty(mongoDbHost string, mongoDbPort int, awsAccessKey string, awsSecretKey string, bucketName string, dataFolder string, s3Domain string) {
@@ -154,24 +103,6 @@ func addtoArchive(path string, fileInfo os.FileInfo, err error) error {
 
 	info.Println("Added file " + path + " to archive.")
 	return nil
-}
-
-func (service *mongoService) lockDb() {
-	info.Println("Attempting to LOCK DB...")
-	err := service.session.FsyncLock()
-	if err != nil {
-		log.Panic("Cannot LOCK DB: "+err.Error(), err)
-	}
-	info.Println("DB LOCK command successfully executed.")
-}
-
-func (service *mongoService) unlockDb() {
-	info.Println("Attempting to UNLOCK DB...")
-	err := service.session.FsyncUnlock()
-	if err != nil {
-		log.Panic("Cannot LOCK DB: "+err.Error(), err)
-	}
-	info.Println("DB UNLOCK command successfully executed.")
 }
 
 func initLogs(infoHandle io.Writer, warnHandle io.Writer, panicHandle io.Writer) {
