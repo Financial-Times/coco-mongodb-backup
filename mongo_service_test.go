f7f7fc1823332e3fcfb4c27d9abe84fb9d6a1b8a
Keith Hatton <keith.hatton@ft.com>
2016-08-08 11:49:49 +0100

    Fix casting and introduce test cases.
    []interface{} can't be cast to []string.
    Refactor and add test cases for isNodeForBackup.

---
+++ mongo_service_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const TEMPLATE = `{
+        "ismaster" : %t,
+        "secondary" : %t,
+        "hosts" : [
+                "%s",
+                "%s",
+                "%s"
+        ],
+        "primary" : "%s",
+        "me" : "%s"
+}`
+const PRIMARY = "primary"
+const SEC1 = "secondary1"
+const SEC2 = "secondary2"
+
+func TestMasterIsNotNodeForBackup(t *testing.T) {
+	in := strings.NewReader(fmt.Sprintf(TEMPLATE, true, false, SEC1, SEC2, PRIMARY, PRIMARY, PRIMARY))
+	dec := json.NewDecoder(in)
+	response := make(map[string]interface{})
+	dec.Decode(&response)
+
+	v, info := isNodeForBackup(response)
+
+	assert.False(t, v, "isNodeForBackup")
+	assert.Equal(t, PRIMARY, info.primary, "primary")
+	assert.Equal(t, PRIMARY, info.node, "node")
+	assert.Equal(t, SEC1, info.secondaries[0], "first secondary")
+	assert.Equal(t, SEC2, info.secondaries[1], "other secondary")
+}
+
+func TestLowestSecondaryIsNodeForBackup(t *testing.T) {
+	in := strings.NewReader(fmt.Sprintf(TEMPLATE, false, true, SEC1, PRIMARY, SEC2, PRIMARY, SEC1))
+	dec := json.NewDecoder(in)
+	response := make(map[string]interface{})
+	dec.Decode(&response)
+
+	v, info := isNodeForBackup(response)
+
+	assert.True(t, v, "isNodeForBackup")
+	assert.Equal(t, PRIMARY, info.primary, "primary")
+	assert.Equal(t, SEC1, info.node, "node")
+	assert.Equal(t, SEC1, info.secondaries[0], "first secondary")
+	assert.Equal(t, SEC2, info.secondaries[1], "other secondary")
+}
+
+func TestOtherSecondaryIsNotNodeForBackup(t *testing.T) {
+	in := strings.NewReader(
+		fmt.Sprintf(TEMPLATE, false, true, SEC2, PRIMARY, SEC1, PRIMARY, SEC2))
+	dec := json.NewDecoder(in)
+	response := make(map[string]interface{})
+	dec.Decode(&response)
+
+	v, info := isNodeForBackup(response)
+
+	assert.False(t, v, "isNodeForBackup")
+	assert.Equal(t, PRIMARY, info.primary, "primary")
+	assert.Equal(t, SEC2, info.node, "node")
+	assert.Equal(t, SEC1, info.secondaries[0], "first secondary")
+	assert.Equal(t, SEC2, info.secondaries[1], "other secondary")
+}
